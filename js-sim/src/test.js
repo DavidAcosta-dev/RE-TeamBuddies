7d353b97b655318e367b15badf328d362fa808b8
DavidAcosta-dev <davidacosta.dev@gmail.com>
2025-09-25 11:28:43 -0700

    removed old prototypes of remake

--- js-sim/src/test.js
+++
@@ -1,6 +0,0 @@
-import { simulateNFrames } from './index.js';
-
-// Tiny smoke test to validate the scaffold runs
-const out = simulateNFrames(3, () => ({ ax: 0.5, ay: 0 }), { accelScale: 1 });
-console.log('frames', out.length);
-console.log('last', out[out.length - 1]);
